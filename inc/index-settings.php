8e0380d48035f746b4fc29fd1aa2294c94df9c9f
Khuyen tran <khuyen.tran@360life.vn>
2020-09-28 07:25:03 +0700

    con nhieu thu qua

--- inc/index-settings.php
+++ inc/index-settings.php
@@ -6,19 +6,32 @@ if( function_exists('acf_add_options_page') ) {
 		'menu_title'	=> 'Theme Settings',
 		'menu_slug' 	=> 'theme-general-settings',
 		'capability'	=> 'edit_posts',
-		'redirect'		=> false
+		// 'redirect'		=> false,
+		'position'      => false,
+		'icon_url'      => false
 	));
 	
-	// acf_add_options_sub_page(array(
-	// 	'page_title' 	=> 'Theme Header Settings',
-	// 	'menu_title'	=> 'Header',
-	// 	'parent_slug'	=> 'theme-general-settings',
-	// ));
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Main Picture Setting',
+		'menu_title'	=> 'Main Picture',
+		'parent_slug'	=> 'theme-general-settings',
+	));
 	
-	// acf_add_options_sub_page(array(
-	// 	'page_title' 	=> 'Theme Footer Settings',
-	// 	'menu_title'	=> 'Footer',
-	// 	'parent_slug'	=> 'theme-general-settings',
-	// ));
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'About Me Settings',
+		'menu_title'	=> 'About Me',
+		'parent_slug'	=> 'theme-general-settings',
+	));
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'My Story Settings',
+		'menu_title'	=> 'My Story',
+		'parent_slug'	=> 'theme-general-settings',
+	));
+
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Reaching Me Out Settings',
+		'menu_title'	=> 'Reaching Me Out',
+		'parent_slug'	=> 'theme-general-settings',
+	));
 	
 }
---
+++ template-parts/works.php
@@ -0,0 +1,240 @@
+<div class="bl-panel-items" id="bl-panel-work-items">
+    <!-- Project Starts -->
+    <div data-panel="panel-1">
+        <div class="row">
+            <!-- Project Main Content Starts -->
+            <div class="col s12 l6 xl6 section-padding section-padding-right-none">
+                <img class="responsive-img" src="http://via.placeholder.com/535x400" alt="project" />
+            </div>
+            <!-- Project Main Content Ends -->
+            <!-- Project Details Starts -->
+            <div class="col s12 l6 xl6 section-padding">
+                <h3 class="font-weight-700 uppercase">Image Project</h3>
+                <ul class="project-details second-font">
+                    <li><i class="fa fa-user"></i><span class="font-weight-700"> Client </span>: <span class="font-weight-400 uppercase">Envato</span></li>
+                    <li><i class="fa fa-calendar-o"></i><span class="font-weight-700"> Start Date </span>: <span class="font-weight-400 uppercase">02/11/2017</span></li>
+                    <li><i class="fa fa-calendar-check-o"></i><span class="font-weight-700"> End Date </span>: <span class="font-weight-400 uppercase">02/08/2018</span></li>
+                    <li><i class="fa fa-cogs"></i> <span class="font-weight-700"> Used Technologies</span> : <span class="font-weight-400 uppercase">php, html, css, javascript</span></li>
+                </ul>
+                <hr>
+                <a href="javascript:void(0)" class="waves-effect waves-light btn font-weight-700" onclick="openNav()">Preview <i class="fa fa-external-link"></i></a>
+            </div>
+            <!-- Project Details Ends -->
+        </div>
+    </div>
+    <!-- Project Ends -->
+    <!-- Project Starts -->
+    <div data-panel="panel-2">
+        <div class="row">
+            <!-- Project Main Content Starts -->
+            <div class="col s12 l6 xl6 section-padding section-padding-right-none">
+                <div class="carousel carousel-slider">
+                    <a class="carousel-item" href="#one!"><img class="responsive-img" src="http://via.placeholder.com/535x400" alt="project" ></a>
+                    <a class="carousel-item" href="#two!"><img class="responsive-img" src="http://via.placeholder.com/535x400" alt="project" ></a>
+                    <a class="carousel-item" href="#three!"><img class="responsive-img" src="http://via.placeholder.com/535x400" alt="project" ></a>
+                    <a class="carousel-item" href="#four!"><img class="responsive-img" src="http://via.placeholder.com/535x400" alt="project" ></a>
+                </div>
+            </div>
+            <!-- Project Main Content Ends -->
+            <!-- Project Details Starts -->
+            <div class="col s12 l6 xl6 section-padding">
+                <h3 class="font-weight-700 uppercase">Slider Project</h3>
+                <ul class="project-details second-font">
+                    <li><i class="fa fa-user"></i><span class="font-weight-700"> Client </span>: <span class="font-weight-400 uppercase">Themeforest</span></li>
+                    <li><i class="fa fa-calendar-o"></i><span class="font-weight-700"> Start Date </span>: <span class="font-weight-400 uppercase">02/11/2017</span></li>
+                    <li><i class="fa fa-calendar-check-o"></i><span class="font-weight-700"> End Date </span>: <span class="font-weight-400 uppercase">02/08/2018</span></li>
+                    <li><i class="fa fa-cogs"></i> <span class="font-weight-700"> Used Technologies</span> : <span class="font-weight-400 uppercase">php, html, css, javascript</span></li>
+                </ul>
+                <hr>
+                <a href="javascript:void(0)" class="waves-effect waves-light btn font-weight-700" onclick="openNav()">Preview <i class="fa fa-external-link"></i></a>
+            </div>
+            <!-- Project Details Ends -->
+        </div>
+    </div>
+    <!-- Project Ends -->
+    <!-- Project Starts -->
+    <div data-panel="panel-3">
+        <div class="row">
+            <!-- Project Main Content Starts -->
+            <div class="col s12 l6 xl6 section-padding section-padding-right-none">
+                <div class="videocontainer">
+                    <iframe class="youtube-video" src="https://www.youtube.com/embed/7e90gBu4pas?enablejsapi=1&version=3&playerapiid=ytplayer" allowfullscreen></iframe>
+                </div>
+            </div>
+            <!-- Project Main Content Ends -->
+            <!-- Project Details Starts -->
+            <div class="col s12 l6 xl6 section-padding">
+                <h3 class="font-weight-700 uppercase">Youtube Video</h3>
+                <ul class="project-details second-font">
+                    <li><i class="fa fa-user"></i><span class="font-weight-700"> Client </span>: <span class="font-weight-400 uppercase">Photodune</span></li>
+                    <li><i class="fa fa-calendar-o"></i><span class="font-weight-700"> Start Date </span>: <span class="font-weight-400 uppercase">02/11/2017</span></li>
+                    <li><i class="fa fa-calendar-check-o"></i><span class="font-weight-700"> End Date </span>: <span class="font-weight-400 uppercase">02/08/2018</span></li>
+                    <li><i class="fa fa-cogs"></i> <span class="font-weight-700"> Used Technologies</span> : <span class="font-weight-400 uppercase">Adobe After Effects, Adobe Audition</span></li>
+                </ul>
+                <hr>
+                <a href="javascript:void(0)" class="waves-effect waves-light btn font-weight-700" onclick="openNav()">Preview <i class="fa fa-external-link"></i></a>
+            </div>
+            <!-- Project Details Ends -->
+        </div>
+    </div>
+    <!-- Project Ends -->
+    <!-- Project Starts -->
+    <div data-panel="panel-4">
+        <div class="row">
+            <!-- Project Main Content Starts -->
+            <div class="col s12 l6 xl6 section-padding section-padding-right-none">
+                <video id="video" class="responsive-video" controls poster="http://via.placeholder.com/561x315">
+                    <source src=<?php echo THEME_PATH."images/projects/video/video.mp4" ?> type="video/mp4">
+                </video>
+            </div>
+            <!-- Project Main Content Ends -->
+            <!-- Project Details Starts -->
+            <div class="col s12 l6 xl6 section-padding">
+                <h3 class="font-weight-700 uppercase">Local Video</h3>
+                <ul class="project-details second-font">
+                    <li><i class="fa fa-user"></i><span class="font-weight-700"> Client </span>: <span class="font-weight-400 uppercase">Videohive</span></li>
+                    <li><i class="fa fa-calendar-o"></i><span class="font-weight-700"> Start Date </span>: <span class="font-weight-400 uppercase">02/11/2017</span></li>
+                    <li><i class="fa fa-calendar-check-o"></i><span class="font-weight-700"> End Date </span>: <span class="font-weight-400 uppercase">02/08/2018</span></li>
+                    <li><i class="fa fa-cogs"></i> <span class="font-weight-700"> Used Technologies</span> : <span class="font-weight-400 uppercase">Adobe After Effects, Movie Maker</span></li>
+                </ul>
+                <hr>
+                <a href="javascript:void(0)" class="waves-effect waves-light btn font-weight-700" onclick="openNav()">Preview <i class="fa fa-external-link"></i></a>
+            </div>
+            <!-- Project Details Ends -->
+        </div>
+    </div>
+    <!-- Project Ends -->
+    <!-- Project Starts -->
+    <div data-panel="panel-5">
+        <div class="row">
+            <!-- Project Main Content Starts -->
+            <div class="col s12 l6 xl6 section-padding section-padding-right-none">
+                <img class="responsive-img" src="http://via.placeholder.com/535x400" alt="project" />
+            </div>
+            <!-- Project Main Content Ends -->
+            <!-- Project Details Starts -->
+            <div class="col s12 l6 xl6 section-padding">
+                <h3 class="font-weight-700 uppercase">Image Project</h3>
+                <ul class="project-details second-font">
+                    <li><i class="fa fa-user"></i><span class="font-weight-700"> Client </span>: <span class="font-weight-400 uppercase">Graphicriver</span></li>
+                    <li><i class="fa fa-calendar-o"></i><span class="font-weight-700"> Start Date </span>: <span class="font-weight-400 uppercase">02/11/2017</span></li>
+                    <li><i class="fa fa-calendar-check-o"></i><span class="font-weight-700"> End Date </span>: <span class="font-weight-400 uppercase">02/08/2018</span></li>
+                    <li><i class="fa fa-cogs"></i> <span class="font-weight-700"> Used Technologies</span> : <span class="font-weight-400 uppercase">Adobe Photoshop, Gimp</span></li>
+                </ul>
+                <hr>
+                <a href="javascript:void(0)" class="waves-effect waves-light btn font-weight-700" onclick="openNav()">Preview <i class="fa fa-external-link"></i></a>
+            </div>
+            <!-- Project Details Ends -->
+        </div>
+    </div>
+    <!-- Project Ends -->
+    <!-- Project Starts -->
+    <div data-panel="panel-6">
+        <div class="row">
+            <!-- Project Main Content Starts -->
+            <div class="col s12 l6 xl6 section-padding section-padding-right-none">
+                <img class="responsive-img" src="http://via.placeholder.com/535x400" alt="project" />
+            </div>
+            <!-- Project Main Content Ends -->
+            <!-- Project Details Starts -->
+            <div class="col s12 l6 xl6 section-padding">
+                <h3 class="font-weight-700 uppercase">Image Project</h3>
+                <ul class="project-details second-font">
+                    <li><i class="fa fa-user"></i><span class="font-weight-700"> Client </span>: <span class="font-weight-400 uppercase">Activeden</span></li>
+                    <li><i class="fa fa-calendar-o"></i><span class="font-weight-700"> Start Date </span>: <span class="font-weight-400 uppercase">02/11/2017</span></li>
+                    <li><i class="fa fa-calendar-check-o"></i><span class="font-weight-700"> End Date </span>: <span class="font-weight-400 uppercase">02/08/2018</span></li>
+                    <li><i class="fa fa-cogs"></i> <span class="font-weight-700"> Used Technologies</span> : <span class="font-weight-400 uppercase">Adobe Flash, Paint</span></li>
+                </ul>
+                <hr>
+                <a href="javascript:void(0)" class="waves-effect waves-light btn font-weight-700" onclick="openNav()">Preview <i class="fa fa-external-link"></i></a>
+            </div>
+            <!-- Project Details Ends -->
+        </div>
+    </div>
+    <!-- Project Ends -->
+    <!-- Project Starts -->
+    <div data-panel="panel-7">
+        <div class="row">
+            <!-- Project Main Content Starts -->
+            <div class="col s12 l6 xl6 section-padding section-padding-right-none">
+                <img class="responsive-img" src="http://via.placeholder.com/535x400" alt="project" />
+            </div>
+            <!-- Project Main Content Ends -->
+            <!-- Project Details Starts -->
+            <div class="col s12 l6 xl6 section-padding">
+                <h3 class="font-weight-700 uppercase">Image Project</h3>
+                <ul class="project-details second-font">
+                    <li><i class="fa fa-user"></i><span class="font-weight-700"> Client </span>: <span class="font-weight-400 uppercase">3D Ocean</span></li>
+                    <li><i class="fa fa-calendar-o"></i><span class="font-weight-700"> Start Date </span>: <span class="font-weight-400 uppercase">02/11/2017</span></li>
+                    <li><i class="fa fa-calendar-check-o"></i><span class="font-weight-700"> End Date </span>: <span class="font-weight-400 uppercase">02/08/2018</span></li>
+                    <li><i class="fa fa-cogs"></i> <span class="font-weight-700"> Used Technologies</span> : <span class="font-weight-400 uppercase">3DS Max, Adobe Photoshop</span></li>
+                </ul>
+                <hr>
+                <a href="javascript:void(0)" class="waves-effect waves-light btn font-weight-700" onclick="openNav()">Preview <i class="fa fa-external-link"></i></a>
+            </div>
+            <!-- Project Details Ends -->
+        </div>
+    </div>
+    <!-- Project Ends -->
+    <!-- Project Starts -->
+    <div data-panel="panel-8">
+        <div class="row">
+            <!-- Project Main Content Starts -->
+            <div class="col s12 l6 xl6 section-padding section-padding-right-none">
+                <img class="responsive-img" src="http://via.placeholder.com/535x400" alt="project" />
+            </div>
+            <!-- Project Main Content Ends -->
+            <!-- Project Details Starts -->
+            <div class="col s12 l6 xl6 section-padding">
+                <h3 class="font-weight-700 uppercase">Image Project</h3>
+                <ul class="project-details second-font">
+                    <li><i class="fa fa-user"></i><span class="font-weight-700"> Client </span>: <span class="font-weight-400 uppercase">Audiojungle</span></li>
+                    <li><i class="fa fa-calendar-o"></i><span class="font-weight-700"> Start Date </span>: <span class="font-weight-400 uppercase">02/11/2017</span></li>
+                    <li><i class="fa fa-calendar-check-o"></i><span class="font-weight-700"> End Date </span>: <span class="font-weight-400 uppercase">02/08/2018</span></li>
+                    <li><i class="fa fa-cogs"></i> <span class="font-weight-700"> Used Technologies</span> : <span class="font-weight-400 uppercase">Adobe Audition, Adobe Premiere</span></li>
+                </ul>
+                <hr>
+                <a href="javascript:void(0)" class="waves-effect waves-light btn font-weight-700" onclick="openNav()">Preview <i class="fa fa-external-link"></i></a>
+            </div>
+            <!-- Project Details Ends -->
+        </div>
+    </div>
+    <!-- Project Ends -->
+    <!-- Project Starts -->
+    <div data-panel="panel-9">
+        <div class="row">
+            <!-- Project Main Content Starts -->
+            <div class="col s12 l6 xl6 section-padding section-padding-right-none">
+                <img class="responsive-img" src="http://via.placeholder.com/535x400" alt="project" />
+            </div>
+            <!-- Project Main Content Ends -->
+            <!-- Project Details Starts -->
+            <div class="col s12 l6 xl6 section-padding">
+                <h3 class="font-weight-700 uppercase">Image Project</h3>
+                <ul class="project-details second-font">
+                    <li><i class="fa fa-user"></i><span class="font-weight-700"> Client </span>: <span class="font-weight-400 uppercase">Audiojungle</span></li>
+                    <li><i class="fa fa-calendar-o"></i><span class="font-weight-700"> Start Date </span>: <span class="font-weight-400 uppercase">02/11/2017</span></li>
+                    <li><i class="fa fa-calendar-check-o"></i><span class="font-weight-700"> End Date </span>: <span class="font-weight-400 uppercase">02/08/2018</span></li>
+                    <li><i class="fa fa-cogs"></i> <span class="font-weight-700"> Used Technologies</span> : <span class="font-weight-400 uppercase">Adobe Audition, Adobe Premiere</span></li>
+                </ul>
+                <hr>
+                <a href="javascript:void(0)" class="waves-effect waves-light btn font-weight-700" onclick="openNav()">Preview <i class="fa fa-external-link"></i></a>
+            </div>
+            <!-- Project Details Ends -->
+        </div>
+    </div>
+    <!-- Project Ends -->
+    <!-- Portfolio Navigation Starts -->
+    <nav>
+        <!-- Previous Work Icon Starts -->
+        <span class="control-button bl-previous-work"><i class="fa fa-angle-left"></i></span>
+        <!-- Previous Work Icon Ends -->
+        <!-- Close Work Icon Starts -->
+        <img alt="close" src=<?php echo THEME_PATH."images/close-button.png" ?> class="control-button bl-icon-close" />
+        <!-- Close Work Icon Ends -->
+        <!-- Next Work Icon Starts -->
+        <span class="control-button bl-next-work"><i class="fa fa-angle-right"></i></span>
+        <!-- Previous Work Icon Ends -->
+    </nav>
+    <!-- Portfolio Navigation Ends -->
+</div>
